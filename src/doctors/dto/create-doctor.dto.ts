e5b5e00c07f7aebf639f69975349cb63fed25881
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-12-04 11:53:04 -0300

    Feat: Doctor Dto Config with class-validator

--- src/doctors/dto/create-doctor.dto.ts
+++ src/doctors/dto/create-doctor.dto.ts
@@ -1,1 +1,32 @@
-export class CreateDoctorDto {}
+import { IsArray, IsNotEmpty, IsNumberString, IsString, Length } from 'class-validator';
+
+export class CreateDoctorDto {
+    @IsNotEmpty()
+    @IsString()
+    @Length(3, 120)
+    name: string;
+
+    @IsNotEmpty()
+    @IsNumberString()  
+    @Length(7, 7)
+    crm: string;
+
+    @IsNotEmpty()
+    @IsNumberString()
+    @Length(10, 10)
+    phone: string;
+
+    @IsNotEmpty()
+    @IsNumberString()
+    @Length(11, 11)
+    cellphone: string;
+
+    @IsNotEmpty()
+    @IsNumberString()
+    @Length(8, 8)
+    zipCode: string;
+
+    @IsNotEmpty()
+    @IsArray()
+    specialties: string[];
+}
