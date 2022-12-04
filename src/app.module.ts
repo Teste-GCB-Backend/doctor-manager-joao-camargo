8683c1fc3c122ad77a4b2aa7121324e8ae6c2c3c
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-12-04 11:44:08 -0300

    Feat: create Doctors resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DoctorsModule } from './doctors/doctors.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AppService } from './app.service';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
     }),
+    DoctorsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
