c7674f1f0de86b18e382534b74fa611ff9b00a4e
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-12-04 11:48:51 -0300

    Feat: create doctor entity

--- src/doctors/entities/doctor.entity.ts
+++ src/doctors/entities/doctor.entity.ts
@@ -1,1 +1,21 @@
-export class Doctor {}
+import { PrimaryGeneratedColumn, Column }  from "typeorm"
+
+export class Doctor {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({type: 'varchar', length: 120, nullable: false})
+    name: string;
+
+    @Column({type: 'int', width: 7, unique: true, nullable: false})
+    crm: number;
+
+    @Column({ type: 'varchar', length: 10, nullable: false })
+    landline: number;
+
+    @Column({ type: 'varchar', length: 11, nullable: false })
+    cellphone: number;
+
+    @Column({ type: 'int', nullable: false, unique: true, select: false })
+    address: number;
+}
