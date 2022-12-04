172c930ac3360d766e16da31d02fdf9e9b9cc8ef
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-12-04 12:12:28 -0300

    Feat: create migration address foreign key

---
+++ src/database/migrations/1670166689090-AddressAddForeignKey.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ForeignKey1668379780281 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE doctors
+        ADD CONSTRAINT fk_adresses_doctors
+        FOREIGN KEY address_doctor(addressId)
+        REFERENCES addresses(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE;`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE doctors
+        DROP CONSTRAINT fk_adresses_doctors;`);
+    }
+
+}
